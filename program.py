abb77c3500205cfbeb7c1a221c1ea845a1c18f6c
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-05 01:38:50 +0500

    program.py is the correct file, teammate did wrong file name there

--- program.py
+++ program.py
@@ -32,7 +32,7 @@ def results(data: Data):
 
 def main():
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("program:app", host="0.0.0.0", port=8000, reload=True)
 if __name__ == "__main__":
     main()
 
