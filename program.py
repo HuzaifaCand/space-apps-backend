c1aca81bbe1eabd66be59beb004ae26b32f4ee72
Syed Huzaifa Hassan <s.huzaifa.hassan12@gmail.com>
2025-10-05 01:48:25 +0500

    more deployment problems

--- program.py
+++ program.py
@@ -32,7 +32,7 @@ def results(data: Data):
 
 def main():
     import uvicorn
-    uvicorn.run("program:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("program:app", host="127.0.0.1", port=8000, reload=True)
 if __name__ == "__main__":
     main()
 
